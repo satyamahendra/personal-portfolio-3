0fdc00c1832693d8b0fae48db6866bac83017581
Megatronhehe <satyamahendra09@gmail.com>
2023-12-26 18:58:55 +0700

    feat: navigate to skills as next page in main page

--- src/pages/Main/MiddleSection.tsx
+++ src/pages/Main/MiddleSection.tsx
@@ -1,15 +1,11 @@
-import {
-	PiDiamondFill,
-	PiDiamondLight,
-	PiDiamondsFourLight,
-	PiDiamondsFourFill,
-} from "react-icons/pi";
+import { PiCaretUpLight } from "react-icons/pi";
 
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 export default function MiddleSection() {
 	return (
-		<div className="w-full flex item-center justify-between sm:p-12 p-4">
+		<div className="w-full flex justify-between sm:p-12 p-4">
 			<section className="flex flex-col justify-center">
 				<nav className="flex flex-col gap-8 text-2xl sm:text-4xl duration-200">
 					<FaLinkedin />
@@ -18,13 +14,16 @@ export default function MiddleSection() {
 				</nav>
 			</section>
 
-			<section className="flex flex-col justify-center">
-				<div className="flex flex-col gap-8 text-xl">
-					<PiDiamondLight />
-					<PiDiamondFill />
-					<PiDiamondsFourLight />
-					<PiDiamondsFourFill />
-				</div>
+			<section className="flex flex-col justify-center mt-32">
+				<Link
+					to="/skills"
+					className="group text-gray-500  rotate-90 origin-top-right flex  items-center gap-4"
+				>
+					<div className="border p-1 border-gray-400 group-hover:border-black duration-200 group-hover:bg-black group-hover:text-gray-200">
+						<PiCaretUpLight />
+					</div>
+					<span className="group-hover">Next page</span>
+				</Link>
 			</section>
 		</div>
 	);
