0fcfbb708b4c18dc3290f0fa6b9a5b1aa730d3ab
Megatronhehe <satyamahendra09@gmail.com>
2023-12-27 09:01:33 +0700

    style: responsiveness on skills page nav

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -5,9 +5,9 @@ import { PiGearLight } from "react-icons/pi";
 export default function Skills() {
 	return (
 		<main className="flex justify-center h-screen">
-			<div className="flex w-full max-w-3xl py-32 pr-4 sm:pr-8">
+			<div className="flex w-full max-w-3xl pt-32 pb-12 pr-4 sm:pr-8">
 				<Outlet />
-				<section className="flex flex-col justify-center w-2/5 gap-4 px-4 border-l border-gray-400">
+				<section className="flex flex-col justify-center w-2/5 gap-4 px-4 duration-200 border-l border-gray-400 sm:mb-16">
 					<h2 className="flex items-center gap-2 pb-4 text-xl border-b border-gray-400">
 						<PiGearLight /> Skills
 					</h2>
